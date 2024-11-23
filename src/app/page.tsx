71f2148f9d55b24b06bdf65bbb9af338eebb2003
AryaEm <ary4maulanaa@gmail.com>
2024-11-23 19:20:34 +0700

    background test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-// import Image from "next/image";
+import Image from "next/image";
 import Navbar from "@/components/nav";
 import Hero from "@/components/hero"
+import podium from "@/assets/3d podium.svg"
 
 export default function Home() {
   return (
-    <section>
+    <section className="h-dvh w-full">
+      <Image src={podium} alt="" className="h-full w-full object-cover absolute -z-[1]"/>
       <Hero></Hero>
       <Navbar></Navbar>
     </section>
